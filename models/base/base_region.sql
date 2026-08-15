b0816cc2efc1d25a717025c3ac69391d5ba1bf58
pavan.talla@yash.com <pavan.talla@yash.com>
2026-08-15 05:25:18 +0000

    base_layer_setup_done

---
+++ models/base/base_region.sql
@@ -0,0 +1,11 @@
+{{
+    config(
+        materialized = 'view'
+    )
+}}
+select
+    r_regionkey as region_key,
+    r_name as region_name,
+    r_comment as region_comment
+from
+    {{ source('tpch', 'region') }}
